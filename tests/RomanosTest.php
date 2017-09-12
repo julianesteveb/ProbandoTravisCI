81313fbbc352efbb124cb9bd2a08b22f6b968aee
julianesteveb <31454104+julianesteveb@users.noreply.github.com>
2017-09-12 17:25:24 -0300

    Update RomanosTest.php

--- tests/RomanosTest.php
+++ tests/RomanosTest.php
@@ -4005,7 +4005,7 @@ $numeros = [
 class romantest extends TestCase {
   public function testNum() {
     for ($t = 1; $t <= 3999; ++$t){
-      $valor= new romans($t);
+      $num= new romans($t);
       $this->assertEquals($num->calcular($num->valor), $numeros[$t]);
 }
 
